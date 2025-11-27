44bd8f634f59ce1c08fc694a3f50878c21f5d97f
Rumato Estorsky <rumatoest@users.noreply.github.com>
2025-11-27 11:57:51 +0300

    Gradle updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,16 @@ repositories {
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:1.7.+")
+    implementation("org.slf4j:slf4j-api:2.0.+")
 
-    testImplementation("org.assertj:assertj-core:3.24.+")
-    testImplementation("org.testng:testng:7.7.1")
+    testImplementation("org.assertj:assertj-core:3.27.+")
+    testImplementation("org.testng:testng:7.11.+")
 }
 
 tasks {
     java {
+        withJavadocJar()
+        withSourcesJar()
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -26,22 +28,6 @@ tasks {
         testLogging.showStandardStreams = true
     }
 
-    val sourcesJar by creating(Jar::class) {
-        dependsOn(classes)
-        classifier = "sources"
-        from(sourceSets["main"].allSource)
-    }
-
-    val javadocJar by creating(Jar::class) {
-        dependsOn(javadoc)
-        classifier = "javadoc"
-        from(javadoc.get().destinationDir)
-    }
-
-    artifacts {
-        add("archives", sourcesJar)
-        add("archives", javadocJar)
-    }
 }
 
 publishing {
@@ -50,8 +36,6 @@ publishing {
             version = version
             groupId = artifactGroup
             from(components["java"])
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
         }
     }
 }
